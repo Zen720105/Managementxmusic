5ba753acb1a6e614744aa696bdc6ed4050eebb11
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-04 18:32:49 +0530

    Update stream.py

--- VIPMUSIC/utils/stream/stream.py
+++ VIPMUSIC/utils/stream/stream.py
@@ -28,7 +28,7 @@ from VIPMUSIC.utils.inline.play import queue_markup, stream_markup, telegram_mar
 from VIPMUSIC.utils.inline.playlist import close_markup
 from VIPMUSIC.utils.pastebin import VIPbin
 from VIPMUSIC.utils.stream.queue import put_queue, put_queue_index
-from VIPMUSIC.utils.thumbnails import gen_qthumb, get_thumb
+from VIPMUSIC.utils.thumbnails import get_thumb
 
 
 async def stream(
@@ -170,7 +170,7 @@ async def stream(
                 "video" if video else "audio",
             )
             position = len(db.get(chat_id)) - 1
-            qimg = await gen_qthumb(vidid)
+            qimg = await get_thumb(vidid)
             button = queue_markup(_, vidid, chat_id)
             run = await app.send_photo(
                 original_chat_id,
