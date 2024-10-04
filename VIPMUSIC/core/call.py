78daafe70f29130d85c889c8c773345095cc8269
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-04 17:30:34 +0530

    Update call.py

--- VIPMUSIC/core/call.py
+++ VIPMUSIC/core/call.py
@@ -54,7 +54,7 @@ from VIPMUSIC.utils.exceptions import AssistantErr
 from VIPMUSIC.utils.formatters import check_duration, seconds_to_min, speed_converter
 from VIPMUSIC.utils.inline.play import stream_markup, telegram_markup
 from VIPMUSIC.utils.stream.autoclear import auto_clean
-from VIPMUSIC.utils.thumbnails import gen_thumb
+from VIPMUSIC.utils.thumbnails import get_thumb
 
 autoend = {}
 counter = {}
