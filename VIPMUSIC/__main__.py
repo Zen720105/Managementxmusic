e69fc0e0e208ae61765bd4d9328b3aee70ae71b3
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-07 17:52:13 +0530

    Update __main__.py

--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -7,15 +7,13 @@
 #
 # All rights reserved.
 import asyncio
-import importlib
 
 from pyrogram import idle
 
 import config
 from config import BANNED_USERS
-from VIPMUSIC import HELPABLE, LOGGER, app, userbot
+from VIPMUSIC import LOGGER, app, userbot
 from VIPMUSIC.core.call import VIP
-from VIPMUSIC.plugins import ALL_MODULES
 from VIPMUSIC.utils.database import get_banned_users, get_gbanned
 
 
@@ -47,15 +45,6 @@ async def init():
         pass
 
     await app.start()
-
-    for all_module in ALL_MODULES:
-        imported_module = importlib.import_module(all_module)
-
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
-    LOGGER("VIPMUSIC.plugins").info("Successfully Imported All Modules ")
-
     await userbot.start()
     await VIP.start()
     await VIP.decorators()
